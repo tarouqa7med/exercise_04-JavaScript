09f8bfc5618505fcf9de7b0c5280a43fc931c8dc
Tarouq Ahmed <tariq.ahmed67006@gmail.com>
2026-09-08 01:41:37 +0300

    ;lk

--- js/main.js
+++ js/main.js
@@ -1,19 +1,37 @@
-
 let plus = document.querySelectorAll(".plus"),
-    close=document.querySelectorAll(".close"),
-    modal = document.querySelector(".Modal");
+  close = document.querySelectorAll(".close"),
+  next = document.querySelectorAll(".next"),
+  prev = document.querySelectorAll(".prev"),
+  modal = document.querySelector(".Modal"),
+  box = document.querySelector(".box");
 
 plus.forEach(function (e) {
   e.addEventListener("click", function () {
     modal.style.display = "block";
-    modal.style.opacity = "1";
-  })
-})
-
-close.forEach(function (e) {
-  e.addEventListener("click", function () {
-    modal.style.display = "none";
-    modal.style.opacity = "0";
-    console.log("ijhkjh")
+    setTimeout(function () {
+      modal.style.opacity = "1";
+    }, 1);
   });
+});
+
+prev.forEach(function (e) {
+  e.addEventListener("click", function () {
+    modal.style.opacity = "0";
+    setTimeout(function () {
+      modal.style.display = "none";
+    }, 1000);
+  });
+});
+
+modal.addEventListener("click", () => {
+  console.log("Modal clicked");
+  modal.style.opacity = "0";
+    setTimeout(function () {
+      modal.style.display = "none";
+    }, 500);
+  });
+
+box.addEventListener("click", (event) => {
+  event.stopPropagation();
+  console.log("Box clicked");
 });
