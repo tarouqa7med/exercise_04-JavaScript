b47054a60f2f6686c7cce63c3edbe2bbe70cf99b
Tarouq Ahmed <tariq.ahmed67006@gmail.com>
2026-09-08 01:18:12 +0300

    lkj

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 
 let plus = document.querySelectorAll(".plus"),
+    close=document.querySelectorAll(".close"),
     modal = document.querySelector(".Modal");
 
 plus.forEach(function (e) {
   e.addEventListener("click", function () {
     modal.style.display = "block";
+    modal.style.opacity = "1";
   })
 })
+
+close.forEach(function (e) {
+  e.addEventListener("click", function () {
+    modal.style.display = "none";
+    modal.style.opacity = "0";
+    console.log("ijhkjh")
+  });
+});
