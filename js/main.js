8cf76cd8f606bae8863d3af63e071e2020b03e5a
Tarouq Ahmed <tariq.ahmed67006@gmail.com>
2026-09-08 01:14:38 +0300

    lkj

--- js/main.js
+++ js/main.js
@@ -1,14 +1,9 @@
 
-let plus_icon = document.getElementById("plus"),
-  modal = document.getElementById("modal");
+let plus = document.querySelectorAll(".plus"),
+    modal = document.querySelector(".Modal");
 
-plus_icon.addEventListener("click", function () {
-  console.log(plus_icon);
-}) 
-
-plus_icon.addEventListener("click", () => {
-  modal.classList.add("active");
-  setTimeout(function () {
-    modal.classList.add("show");
-  }, 1); 
+plus.forEach(function (e) {
+  e.addEventListener("click", function () {
+    modal.style.display = "block";
+  })
 })
